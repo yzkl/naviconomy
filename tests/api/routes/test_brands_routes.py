c160379070b2960d758f90fc52658bed19103f48
Zeke Alagano <yzkl.alagano@gmail.com>
2025-09-12 23:36:07 +0800

    Simplified tests for brands' routes.

--- tests/api/routes/test_brands_routes.py
+++ tests/api/routes/test_brands_routes.py
@@ -3,7 +3,6 @@ from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import models
-from exceptions.exceptions import EntityAlreadyExistsError, EntityDoesNotExistError
 
 URL_PREFIX = "/brand/"
 
@@ -16,33 +15,11 @@ async def setup(async_session: AsyncSession) -> None:
 
 
 @pytest.mark.asyncio
-async def test_create_brand_returns_http_422_for_missing_name(
-    async_client: AsyncClient,
-) -> None:
-    response = await async_client.post(URL_PREFIX, json={})
-
-    assert response.status_code == 422
-
-
-@pytest.mark.asyncio
-async def test_create_brand_returns_http_422_for_invalid_name_type(
+async def test_create_brand_returns_http_422_for_improper_parameter(
     async_client: AsyncClient,
 ) -> None:
     response = await async_client.post(URL_PREFIX, json={"name": False})
-
     assert response.status_code == 422
-
-
-@pytest.mark.asyncio
-async def test_create_brand_raises_EntityAlreadyExistsError(
-    testing_session: AsyncSession,
-    async_client: AsyncClient,
-) -> None:
-    # Seed test brands
-    await setup(testing_session)
-    # Create an existing brand
-    with pytest.raises(EntityAlreadyExistsError):
-        await async_client.post(URL_PREFIX, json={"name": "test brand"})
 
 
 @pytest.mark.asyncio
@@ -50,80 +27,46 @@ async def test_create_brand_regular(
     async_client: AsyncClient,
 ) -> None:
     response = await async_client.post(URL_PREFIX, json={"name": "brand x"})
-
     assert response.status_code == 200
-    assert response.json()["id"] == 1
-    assert response.json()["name"] == "brand x"
 
 
 @pytest.mark.asyncio
 async def test_read_brand_returns_http_422(async_client: AsyncClient) -> None:
     response = await async_client.get(URL_PREFIX + "true")
-
     assert response.status_code == 422
-
-
-@pytest.mark.asyncio
-async def test_read_brand_raises_EntityDoesNotExistError(
-    async_client: AsyncClient,
-) -> None:
-    with pytest.raises(EntityDoesNotExistError):
-        await async_client.get(URL_PREFIX + "1")
 
 
 @pytest.mark.asyncio
 async def test_read_brand_regular(
     testing_session: AsyncSession, async_client: AsyncClient
 ) -> None:
-    # Seed test brands
     await setup(testing_session)
-
     response = await async_client.get(URL_PREFIX + "1")
-
     assert response.status_code == 200
-    assert response.json()["id"] == 1
-    assert response.json()["name"] == "test brand"
 
 
 @pytest.mark.asyncio
-async def test_read_brands_returns_empty_list(async_client: AsyncClient) -> None:
-    response = await async_client.get(URL_PREFIX)
-
-    assert response.status_code == 200
-    assert response.json() == []
-
-
-@pytest.mark.asyncio
-async def test_read_brands_regular(
+async def test_read_brandsr(
     testing_session: AsyncSession, async_client: AsyncClient
 ) -> None:
     # Seed test brands
     await setup(testing_session)
-
     response = await async_client.get(URL_PREFIX)
-
     assert response.status_code == 200
-    assert isinstance(response.json(), list)
-    assert len(response.json()) == 2
-    assert response.json()[0]["name"] == "test brand"
-    assert response.json()[1]["name"] == "other brand"
 
 
 @pytest.mark.asyncio
-async def test_update_brand_returns_http_422_for_invalid_id_value(
+async def test_update_brand_returns_http_422_for_improper_id(
     testing_session: AsyncSession,
     async_client: AsyncClient,
 ) -> None:
-    # Seed test brands
     await setup(testing_session)
-
     response = await async_client.put(URL_PREFIX + "false", json={"name": "some"})
-
     assert response.status_code == 422
 
 
 @pytest.mark.asyncio
-async def test_update_brand_returns_http_422_for_invalid_parameters(
+async def test_update_brand_returns_http_422_for_improper_parameters(
     testing_session: AsyncSession, async_client: AsyncClient
 ) -> None:
     # Seed test brands
@@ -135,67 +78,24 @@ async def test_update_brand_returns_http_422_for_invalid_parameters(
 
 
 @pytest.mark.asyncio
-async def test_update_brand_raises_EntityAlreadyExistsError(
-    testing_session: AsyncSession, async_client: AsyncClient
-) -> None:
-    # Seed test brands
-    await setup(testing_session)
-
-    # Update one brand to match the other
-    with pytest.raises(EntityAlreadyExistsError):
-        await async_client.put(URL_PREFIX + "1", json={"name": "other brand"})
-
-
-@pytest.mark.asyncio
-async def test_update_brand_raises_EntityDoesNotExistError(
-    async_client: AsyncClient,
-) -> None:
-    with pytest.raises(EntityDoesNotExistError):
-        await async_client.put(URL_PREFIX + "503", json={"name": "other brand"})
-
-
-@pytest.mark.asyncio
 async def test_update_brand_regular(
     testing_session: AsyncSession, async_client: AsyncClient
 ) -> None:
-    # Seed test brands
     await setup(testing_session)
-
     result = await async_client.put(URL_PREFIX + "2", json={"name": "brand x"})
-
     assert result.status_code == 200
-    assert result.json()["id"] == 2
-    assert result.json()["name"] == "brand x"
 
 
 @pytest.mark.asyncio
 async def test_delete_brand_returns_http_422(async_client: AsyncClient) -> None:
     response = await async_client.delete(URL_PREFIX + "true")
-
     assert response.status_code == 422
-
-
-@pytest.mark.asyncio
-async def test_delete_brand_raises_EntityDoesNotExistError(
-    async_client: AsyncClient,
-) -> None:
-    with pytest.raises(EntityDoesNotExistError):
-        await async_client.delete(URL_PREFIX + "404")
 
 
 @pytest.mark.asyncio
 async def test_delete_brand_regular(
     testing_session: AsyncSession, async_client: AsyncClient
 ) -> None:
-    # Seed test brands
     await setup(testing_session)
-
     response = await async_client.delete(URL_PREFIX + "1")
-    table_contents = await async_client.get(URL_PREFIX)
-
     assert response.status_code == 200
-    assert response.json()["name"] == "test brand"
-    assert table_contents.status_code == 200
-    assert isinstance(table_contents.json(), list)
-    assert len(table_contents.json()) == 1
-    assert table_contents.json()[0]["name"] == "other brand"
