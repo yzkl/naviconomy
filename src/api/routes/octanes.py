afcb93a02676e2578b2616c93f086cef6c330b8c
Zeke Alagano <yzkl.alagano@gmail.com>
2025-09-12 22:32:57 +0800

    Added post route for octanes.

---
+++ src/api/routes/octanes.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends, Request
+from loguru import logger
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.limiter import limiter
+from crud import octanes
+from database.session import get_db_session
+from schemas import Octane, OctaneCreate
+
+router = APIRouter(prefix="/octane")
+
+
+@router.post("/", response_model=Octane)
+@limiter.limit("60/minute")
+async def create_octane(
+    request: Request, params: OctaneCreate, db: AsyncSession = Depends(get_db_session)
+) -> Octane:
+    logger.info(f"Creating octane: {params}.")
+    result = await octanes.create_octane(params, db)
+    logger.info(f"Created octane: {result}.")
+    return result
