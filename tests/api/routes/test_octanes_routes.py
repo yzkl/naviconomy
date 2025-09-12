5da7f21c468768c5c51e5b97250f10f7d0ecd232
Zeke Alagano <yzkl.alagano@gmail.com>
2025-09-12 22:58:14 +0800

    Added tests for octanes' get routes.

--- tests/api/routes/test_octanes_routes.py
+++ tests/api/routes/test_octanes_routes.py
@@ -3,7 +3,7 @@ from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import models
-from exceptions.exceptions import EntityAlreadyExistsError
+from exceptions.exceptions import EntityAlreadyExistsError, EntityDoesNotExistError
 
 URL_PREFIX = "/octane/"
 
@@ -51,3 +51,52 @@ async def test_create_octane_regular(async_client: AsyncClient) -> None:
 
     assert response.status_code == 200
     assert response.json()["grade"] == 97
+
+
+@pytest.mark.asyncio
+async def test_read_octane_returns_http_422(async_client: AsyncClient) -> None:
+    response = await async_client.get(URL_PREFIX + "true")
+
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_read_octane_raises_EntityDoesNotExistError(
+    async_client: AsyncClient,
+) -> None:
+    with pytest.raises(EntityDoesNotExistError):
+        await async_client.get(URL_PREFIX + "404")
+
+
+@pytest.mark.asyncio
+async def test_read_octane_regular(
+    testing_session: AsyncSession, async_client: AsyncClient
+) -> None:
+    # Seed test octanes
+    await setup(testing_session)
+
+    response = await async_client.get(URL_PREFIX + "1")
+
+    assert response.status_code == 200
+    assert response.json()["id"] == 1
+    assert response.json()["grade"] == 91
+
+
+@pytest.mark.asyncio
+async def test_read_octanes_returns_empty_list(async_client: AsyncClient) -> None:
+    response = await async_client.get(URL_PREFIX)
+
+    assert response.status_code == 200
+    assert response.json() == []
+
+
+@pytest.mark.asyncio
+async def test_read_octanes_regular(
+    testing_session: AsyncSession, async_client: AsyncClient
+) -> None:
+    # Seed test octanes
+    await setup(testing_session)
+
+    response = await async_client.get(URL_PREFIX)
+
+    assert response.status_code == 200
