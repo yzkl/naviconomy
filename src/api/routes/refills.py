f03b847aea42681c03f1493597a8b96dcbfe668d
Zeke Alagano <yzkl.alagano@gmail.com>
2025-09-13 17:50:02 +0800

    Added 'get' routes for refills.

--- src/api/routes/refills.py
+++ src/api/routes/refills.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import APIRouter, Depends, Request
 from loguru import logger
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -18,4 +20,26 @@ async def create_refill(
     logger.info(f"Creating refill: {params}.")
     result = await refills.create_refill(params, db)
     logger.info(f"Created refill: {result}.")
+    return result
+
+
+@router.get("/{id}", response_model=Refill)
+@limiter.limit("60/minute")
+async def read_refill(
+    request: Request, id: int, db: AsyncSession = Depends(get_db_session)
+) -> Refill:
+    logger.info(f"Fetching refill with id: {id}.")
+    result = await refills.read_refill(id, db)
+    logger.info(f"Fethced refill: {result}.")
+    return result
+
+
+@router.get("/", response_model=List[Refill])
+@limiter.limit("60/minute")
+async def read_refills(
+    request: Request, db: AsyncSession = Depends(get_db_session)
+) -> List[Refill]:
+    logger.info("Fetching refills.")
+    result = await refills.read_refills(db)
+    logger.info("Fetched refills.")
     return result
