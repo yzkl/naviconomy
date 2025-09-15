452be12906582e8b81611f54bb41544cffe0e99f
Zeke Alagano <yzkl.alagano@gmail.com>
2025-09-15 10:48:15 +0800

    Added a function that hashes passwords.

---
+++ src/auth/utils.py
@@ -0,0 +1,21 @@
+from bcrypt import gensalt, hashpw
+from pydantic import SecretStr
+
+
+async def get_password_hash(password: SecretStr) -> str:
+    """Generate a bcrpyt hash from a password.
+
+    A random salt is automatically generated and applied during hasing.
+    The resulting hash is returned as a UTF-8 encoded string.
+
+    Parameters
+    ----------
+    password : SecretStr
+        The password to hash.
+
+    Returns
+    -------
+    str
+        A bcrypt hash of the password, encoded as a UTF-8 string.
+    """
+    return hashpw(password.get_secret_value().encode(), gensalt()).decode()
