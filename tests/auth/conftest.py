abf3974366ba2bf9b044ff2e942a1766aa4bb28b
Zeke Alagano <yzkl.alagano@gmail.com>
2025-09-17 00:26:45 +0800

    Added fixtures for database sessions.

--- tests/auth/conftest.py
+++ tests/auth/conftest.py
@@ -1,22 +1,55 @@
+from typing import AsyncGenerator
+
 import pytest
+import pytest_asyncio
 from pydantic import SecretStr
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from models import Base
 
 # Set up test objects
 TEST_DATA = {
     "username": "test-user",
     "email": "email@email.com",
     "password": SecretStr("strongpassword"),
-    "secret_key": SecretStr("strongkey"),
-    "algorithm": SecretStr("HS256"),
-    "register_payload": {
-        "username": "new-user",
-        "email": "new@email.com",
-        "password": "weakpassword",
-    },
 }
 TEST_DATA["token_payload"] = {"sub": TEST_DATA["username"]}
+TEST_DATA["register_payload"] = {
+    "username": TEST_DATA["username"],
+    "email": TEST_DATA["email"],
+    "password": "weakpassword",
+}
 
 
 @pytest.fixture
 def testing_data() -> dict:
     return TEST_DATA.copy()
+
+
+# Set up database
+DATABASE_URL = "postgresql+asyncpg://postgres:strongpassword@localhost:5431/postgres"
+
+
+@pytest_asyncio.fixture(scope="function")
+async def engine() -> AsyncGenerator[AsyncEngine, None]:
+    engine = create_async_engine(DATABASE_URL, future=True, echo=True)
+    yield engine
+    await engine.dispose()
+
+
+@pytest_asyncio.fixture(scope="function")
+async def testing_session(engine: AsyncEngine) -> AsyncGenerator[AsyncSession, None]:
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    async_session = async_sessionmaker(
+        bind=engine, expire_on_commit=False, class_=AsyncSession
+    )
+    async with async_session() as session:
+        yield session
