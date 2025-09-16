2a9b1bf28e37ed9b957e2dee16a401cec9331c60
Zeke Alagano <yzkl.alagano@gmail.com>
2025-09-16 12:58:32 +0800

    Added tests for 'create_access_token'.

--- tests/auth/test_utils.py
+++ tests/auth/test_utils.py
@@ -1,7 +1,14 @@
+from datetime import datetime, timedelta, timezone
+
+import jwt
 from bcrypt import checkpw
 from pydantic import SecretStr
 
-from auth.utils import get_password_hash, verify_password
+from auth.utils import create_access_token, get_password_hash, verify_password
+from config.settings import settings
+
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
 
 
 def test_get_password_hash_returns_valid_hash(testing_data) -> None:
@@ -38,3 +45,46 @@ def test_verify_password_regular(testing_data) -> None:
     hashed_password = get_password_hash(testing_data["password"])
 
     assert verify_password(testing_data["password"], hashed_password)
+
+
+def test_create_access_token_returns_decodeable_token(testing_data) -> None:
+    token = create_access_token(testing_data["token_payload"])
+    decoded = jwt.decode(
+        token, SECRET_KEY.get_secret_value(), ALGORITHM.get_secret_value()
+    )
+
+    assert decoded["sub"] == testing_data["username"]
+    assert "exp" in decoded
+
+
+def test_create_access_token_with_default_expiration(testing_data) -> None:
+    token = create_access_token(testing_data["token_payload"])
+    decoded = jwt.decode(
+        token, SECRET_KEY.get_secret_value(), ALGORITHM.get_secret_value()
+    )
+
+    assert decoded["exp"] == int(
+        (datetime.now(timezone.utc) + timedelta(minutes=15)).timestamp()
+    )
+
+
+def test_create_access_token_with_custom_expiration(testing_data) -> None:
+    token = create_access_token(testing_data["token_payload"], timedelta(minutes=45))
+    decoded = jwt.decode(
+        token, SECRET_KEY.get_secret_value(), ALGORITHM.get_secret_value()
+    )
+
+    assert decoded["exp"] == int(
+        (datetime.now(timezone.utc) + timedelta(minutes=45)).timestamp()
+    )
+
+
+def test_create_access_token_preserves_payload(testing_data) -> None:
+    testing_data["token_payload"].update({"role": "admin"})
+    token = create_access_token(testing_data["token_payload"])
+    decoded = jwt.decode(
+        token, SECRET_KEY.get_secret_value(), ALGORITHM.get_secret_value()
+    )
+
+    for key in testing_data["token_payload"]:
+        assert decoded[key] == testing_data["token_payload"][key]
