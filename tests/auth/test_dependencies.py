99cbf09fd243b2ae42e5ee527418e2190ba00930
Zeke Alagano <yzkl.alagano@gmail.com>
2025-09-17 21:55:32 +0800

    Added tests for 'get_current_active_user'.

--- tests/auth/test_dependencies.py
+++ tests/auth/test_dependencies.py
@@ -4,10 +4,10 @@ import pytest
 from pydantic import SecretStr
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from auth.dependencies import get_current_user
+from auth.dependencies import get_current_active_user, get_current_user
 from auth.models import DBUser, User
 from auth.utils import create_access_token, get_password_hash
-from exceptions.exceptions import InvalidTokenError
+from exceptions.exceptions import InvalidAccountError, InvalidTokenError
 
 
 async def setup(testing_data: dict, async_session: AsyncSession) -> None:
@@ -54,3 +54,28 @@ async def test_get_current_user_regular(testing_data, testing_session) -> None:
     assert user.username == testing_data["username"]
     assert user.email == testing_data["email"]
     assert user.is_active
+
+
+@pytest.mark.asyncio
+async def test_get_current_active_user_raises_InvalidAccountError() -> None:
+    # Create a deactivated user
+    inactive_user = User(
+        username="inactive-user", email="some@email.com", is_active=False
+    )
+
+    with pytest.raises(InvalidAccountError):
+        await get_current_active_user(current_user=inactive_user)
+
+
+@pytest.mark.asyncio
+async def test_get_current_active_user_regular(testing_data) -> None:
+    test_user = User(
+        username=testing_data["username"], email=testing_data["email"], is_active=True
+    )
+
+    active_user = await get_current_active_user(current_user=test_user)
+
+    assert isinstance(active_user, User)
+    assert active_user.username == testing_data["username"]
+    assert active_user.email == testing_data["email"]
+    assert active_user.is_active
