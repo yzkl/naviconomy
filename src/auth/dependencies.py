dea01197ca44e6bf50eda309b9df5366f5e90b28
Zeke Alagano <yzkl.alagano@gmail.com>
2025-09-17 21:32:51 +0800

    Added a function that returns the current active user.

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -8,7 +8,7 @@ from auth.models import User
 from auth.services import get_user_by_username
 from auth.utils import verify_token
 from database.session import get_db_session
-from exceptions.exceptions import InvalidTokenError
+from exceptions.exceptions import InvalidAccountError, InvalidTokenError
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/token")
 
@@ -44,3 +44,28 @@ async def get_current_user(
     if not db_user:
         raise InvalidTokenError("Invalid credentials.")
     return User.model_validate(db_user)
+
+
+async def get_current_active_user(
+    current_user: User = Depends(get_current_user),
+) -> User:
+    """Return the authenticated user as a Pydantic model if their account is active.
+
+    Parameters
+    ----------
+    current_user : User
+        The user currently authenticated via an access token. Injected via `get_current_user`.
+
+    Returns
+    -------
+    User
+        A Pydantic model representing the authenticated user.
+
+    Raises
+    ------
+    InvalidAccountError
+        If the user's account has been disabled or deactivated.
+    """
+    if not current_user.is_active:
+        raise InvalidAccountError("Account has been disabled or deactivated.")
+    return current_user
